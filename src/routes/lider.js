8663de9dd67606a181a437c983f9b3d2c098cbea
josanva777 <johansanchezvalencia@gmail.com>
2025-08-06 11:08:52 -0500

    primera estructura de backend programador de horarios

--- src/routes/lider.js
+++ src/routes/lider.js
@@ -1,24 +1,24 @@
 // src/routes/lider.js
-import { Router } from 'express';
+import { Router } from "express";
 const router = Router();
-import authenticateLider from '../middlewares/authMiddleware.js';
+import authenticateLider from "../middlewares/authMiddleware.js";
 import {
   getHorariosByEmpleadoId,
   createHorario,
   updateHorario,
-  deleteHorario
-} from '../controllers/horariosController.js';
+  deleteHorario,
+} from "../controller/horariosController.js";
 import {
   getObservacionesByEmpleadoId,
   createObservacion,
   updateObservacion,
-  deleteObservacion
-} from '../controllers/observacionesController.js';
-import supabase from '../services/supabase.service.js';
+  deleteObservacion,
+} from "../controllers/observacionesController.js";
+import supabase from "../services/supabase.service.js";
 
 router.use(authenticateLider);
 
-router.get('/empleados', async (req, res) => {
+router.get("/empleados", async (req, res) => {
   try {
     const { data, error } = await supabase
       .from("empleados")
@@ -27,19 +27,19 @@ router.get('/empleados', async (req, res) => {
     if (error) throw error;
     res.status(200).json(data);
   } catch (error) {
-    console.error('Error al obtener empleados:', error);
-    res.status(500).send('Error al obtener empleados.');
+    console.error("Error al obtener empleados:", error);
+    res.status(500).send("Error al obtener empleados.");
   }
 });
 
-router.get('/horarios/:empleado_id', getHorariosByEmpleadoId);
-router.post('/horarios', createHorario);
-router.put('/horarios/:id', updateHorario);
-router.delete('/horarios/:id', deleteHorario);
+router.get("/horarios/:empleado_id", getHorariosByEmpleadoId);
+router.post("/horarios", createHorario);
+router.put("/horarios/:id", updateHorario);
+router.delete("/horarios/:id", deleteHorario);
 
-router.get('/observaciones/:empleado_id', getObservacionesByEmpleadoId);
-router.post('/observaciones', createObservacion);
-router.put('/observaciones/:id', updateObservacion);
-router.delete('/observaciones/:id', deleteObservacion);
+router.get("/observaciones/:empleado_id", getObservacionesByEmpleadoId);
+router.post("/observaciones", createObservacion);
+router.put("/observaciones/:id", updateObservacion);
+router.delete("/observaciones/:id", deleteObservacion);
 
 export default router;
