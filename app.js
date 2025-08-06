39d2b688933ba91634048d7160b09d1a98ca53ea
josanva777 <johansanchezvalencia@gmail.com>
2025-08-06 13:05:07 -0500

    cors

--- app.js
+++ app.js
@@ -10,11 +10,10 @@ import liderRoutes from "./src/routes/lider.js";
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+// --- Middleware ---
 app.use(cors());
-app.use(express.json({ limit: "50mb" }));
-
-// Manejo global de preflight OPTIONS para CORS
-app.options("*", cors());
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 
 app.use("/api/public", publicRoutes);
 app.use("/api/lider", liderRoutes);
