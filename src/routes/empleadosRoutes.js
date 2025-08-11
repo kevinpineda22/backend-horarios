9ee4f42e63da2281ac49b158999518efd389e4e3
isazaaa <isazamanuel04@gmail.com>
2025-08-11 13:25:26 -0500

    todos los endpoint de empleadosController

--- src/routes/empleadosRoutes.js
+++ src/routes/empleadosRoutes.js
@@ -1,18 +1,21 @@
 // src/routes/empleadosRoutes.js
 import express from 'express';
-import { createEmpleado, uploadEmpleados, toggleEmpleadoStatus } from '../controllers/empleadosController.js';
+import { getEmpleados, createEmpleado, uploadEmpleados, toggleEmpleadoStatus } from '../controllers/empleadosController.js';
 import multer from 'multer';
 
 const router = express.Router();
 const upload = multer(); // Configura multer para manejar archivos en memoria
 
+// Ruta para obtener la lista de todos los empleados
+router.get('/empleados', getEmpleados);
+
 // Ruta para la creación manual de un empleado
-router.post('/', createEmpleado);
+router.post('/empleados', createEmpleado);
 
 // Ruta para la carga masiva de empleados
-router.post('/upload', upload.single('file'), uploadEmpleados);
+router.post('/empleados/upload', upload.single('file'), uploadEmpleados);
 
 // Ruta para actualizar el estado de un empleado
-router.patch('/:id', toggleEmpleadoStatus);
+router.patch('/empleados/:id', toggleEmpleadoStatus);
 
 export default router;
