fd3c97646c027a577593aea59e6ee255df1d32b2
isazaaa <isazamanuel04@gmail.com>
2025-08-11 14:03:28 -0500

    routes correctas de empleadosRoutes

--- src/routes/empleadosRoutes.js
+++ src/routes/empleadosRoutes.js
@@ -6,16 +6,16 @@ import multer from 'multer';
 const router = express.Router();
 const upload = multer(); // Configura multer para manejar archivos en memoria
 
-// Ruta para obtener la lista de todos los empleados
-router.get('/empleados', getEmpleados);
+// Esta ruta ahora responde a GET /api/empleados
+router.get('/', getEmpleados);
 
-// Ruta para la creación manual de un empleado
-router.post('/empleados', createEmpleado);
+// Ruta para la creación manual de un empleado (POST /api/empleados)
+router.post('/', createEmpleado);
 
-// Ruta para la carga masiva de empleados
-router.post('/empleados/upload', upload.single('file'), uploadEmpleados);
+// Ruta para la carga masiva de empleados (POST /api/empleados/upload)
+router.post('/upload', upload.single('file'), uploadEmpleados);
 
-// Ruta para actualizar el estado de un empleado
-router.patch('/empleados/:id', toggleEmpleadoStatus);
+// Ruta para actualizar el estado de un empleado (PATCH /api/empleados/:id)
+router.patch('/:id', toggleEmpleadoStatus);
 
 export default router;
