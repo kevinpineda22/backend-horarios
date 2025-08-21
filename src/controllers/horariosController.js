456b0cb6ee2fc7840ea77b74306c631ac19a0f7f
josanva777 <johansanchezvalencia@gmail.com>
2025-08-21 14:30:35 -0500

    prueba backend 21

--- src/controllers/horariosController.js
+++ src/controllers/horariosController.js
@@ -5,9 +5,8 @@ import {
   getDailyCapacity, // Necesario para la validación en updateHorario
   isoWeekday,
   startOfISOWeek,
-   WEEKLY_LEGAL_LIMIT as WEEKLY_BASE,
-  WEEKLY_EXTRA_LIMIT as WEEKLY_EXTRA,
-  WEEKLY_TOTAL_LIMIT as WEEKLY_TOTAL,
+  WEEKLY_EXTRA,
+  WEEKLY_BASE,
 } from "../utils/schedule.js";
 import { getHolidaySet } from "../utils/holidays.js";
 
