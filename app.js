99cc7aba13e5c22ece43dcbef972042a8e5caeba
josanva777 <johansanchezvalencia@gmail.com>
2025-09-10 17:00:10 -0500

    backend 97

--- app.js
+++ app.js
@@ -11,6 +11,13 @@ import empleadosRoutes from './src/routes/empleadosRoutes.js';
 const app = express();
 const PORT = process.env.PORT;
 
+
+app.use(cors({
+  origin: 'http://localhost:5173',
+  methods: ['GET', 'POST', 'PATCH', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
+
 app.use(cors());
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
